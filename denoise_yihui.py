456385a1fa56e4e24d042b5ef37e773713c0d195
yquek <queky93@gmail.com>
2017-05-24 19:13:21 -0700

    all-in-one DUDE (not tested)

--- denoise_yihui.py
+++ denoise_yihui.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import numpy.linalg as LA
 import matplotlib.pyplot as plt
 from itertools import chain
 
@@ -165,7 +166,15 @@ def denoiseSequence2(input_sequence, k, alphabet, deletion_rate):
 def errorrate_delsub(original,estimate):
     pass
 
-def denoiseSequence3(noisy,k, alphabet): #'noisy' must be a string
+def count_symbols(sequence,alphabet):
+    # count the number of symbols in the sequence and returns a LIST of their frequencies
+    sequence = np.array(sequence)
+    counts = np.zeros(len(alphabet))
+    for i,l in enumerate(alphabet):
+        counts[i] = sum([int(x == l) for x in sequence])
+    return counts
+
+def denoiseSequence3(noisy,k, alphabet, erasedsymbol, ppi, loss): #'noisy' must be a string
     for i in range(k, len(noisy)-k+1):
         if i < len(noisy)-k:
             context = noisy[i-k:i]+noisy[i+1:i+k+1]
@@ -175,26 +184,37 @@ def denoiseSequence3(noisy,k, alphabet): #'noisy' must be a string
                 context_hist[context] = [noisy[i]]
             context_del = noisy[i-k:i+k]
             if context_del in context_hist:
-                context_hist[context_del] += 'N'
+                context_hist[context_del] += erasedsymbol
             else:
-                context_hist[context_del] = ['N']
-
-    for context, symbolswithcontext in contexthist.iteritems():
-        for a in alphabet:
-            new_p = sum(context_hist[context] == a)/(len(context_hist[context])+0.0)
-
-    tosubstitute = {}
-    toinsert = {}
+                context_hist[context_del] = [erasedsymbol]
+    # Create a dictionary that tells you what symbol to insert/replace for each context
+    new_context_hist = {}
+    newalphabet = [i for i in alphabet]
+    newalphabet.extend([erasedsymbol])
+    values = count_symbols(symbollist, newalphabet)
+    for context, symbolswithcontext in context_hist.iteritems():
+        symbollist = list(symbolswithcontext)
+        probhist = np.zeros(len(alphabet)+1)
+        values = count_symbols(symbollist, newalphabet)
+        new_context_hist[context] = values
+    
+    newseq = []
     # Now, make 'noisy' into a list so it's easier to work with
     noisy = list(noisy)
-    noisy_mod = [[i,'N'] for i in noisy]
+    noisy_mod = [[i,erasedsymbol] for i in noisy[k:len(noisy-k)+1]]
+    noisy_mod = itertools.chain(noisy[:k],[erasedsymbol],noisy_mod,noisy[len(noisy)-k+1:])
     noisy_mod = list(chain.from_iterable(noisy_mod))
-    for i in range(2*k,len(noisy_mod)-2*k):
-        if i%2 == 0: # Then it is an actual symbol
-            # Consider if symbol i should be substituted
 
-        else # Then it is a potential deletion location which we have marked with `N's
-
+    for i in range(k,len(noisy_mod)-k):
+        # Consider if symbol i should be substituted
+        context = noisy[j-k:j]+noisy[j+1:j+k+1]
+        emp_probs = new_context_hist[context]
+        front = np.dot(emp_probs.T,LA.inv(ppi))
+        storevals = np.zeros(len(newalphabet))
+        for i in range(len(newalphabet)):
+            storevals[i] = np.dot(front,np.multiply(loss[:,i],emp_probs))
+        newseq.extend(newalphabet[np.argmax(storevals)])
+    return newseq
 # ################################################## Testing the erasure DUDE ####################################################
 
 # n = 10000
@@ -231,11 +251,17 @@ for ii in range(1,n+1):
     Y[ii]=currstate*N[ii]
 X = X[1:]
 Y = Y[1:]
+erasedsymbol = 'N'
 X = ['a' if i==-1 else 'b' for i in X]
-Y = ['a' if i==-1 else 'b' if i==1 else 'E' for i in Y]
-alphabet = ['a','b']
-Yest = denoiseSequence3(Y,k,alphabet)
+Y = ['a' if i==-1 else 'b' if i==1 else erasedsymbol for i in Y]
 
+# Apply the DUDE
+ll = 1
+lossmat = np.array([[0,1,(1+ll)/2.],[1,0,(1+ll)/2.],[ll,ll,0]])
+transitionmat = np.array([[1-ps-pd,ps,pd],[ps,1-ps-pd,pd],[0,0,1]])
+alphabet = ['a','b']
+Yest = denoiseSequence3(Y,k,alphabet,erasedsymbol, transitionmat,lossmat)
+print Yest
 
 ################################################## Graph generation and plotting code ####################################################
 # numpoints = 10
